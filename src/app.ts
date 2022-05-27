1028beaf234e2cdf8425b9c3b6b488f5ea6dc36b
AnJuHwan <wnghks0531@gmail.com>
2022-05-28 07:52:45 +0900

    1

--- src/app.ts
+++ src/app.ts
@@ -22,6 +22,6 @@ app.use('/users', userRouter);
 app.use(express.static('public')); // public폴더 안에있는 모든 리소스를 가져갈 수 있음
 
 app.get('/', (req, res) => {
-  return res.status(200).json({ message: '서버연결!' });
+  return res.status(200).json({ message: '서버연결!!' });
 });
 app.listen(port);
